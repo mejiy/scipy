bb0265b7c728240e0b0584dc59bf02f50fe3fcf9
indra-ipd <indra.ipd@gmail.com>
2021-04-28 15:49:56 +0900

    Updated minimize.py with molnaq

--- scipy/optimize/_minimize.py
+++ scipy/optimize/_minimize.py
@@ -23,7 +23,7 @@ from scipy.sparse.linalg import LinearOperator
 # unconstrained minimization
 from .optimize import (_minimize_neldermead, _minimize_powell, _minimize_cg,_minimize_sr1,
                        _minimize_obfgs,_minimize_onaq,_minimize_olbfgs,_minimize_olnaq,_minimize_adam,
-                       _minimize_adasecant,_minimize_adaQN,_minimize_adaNAQ,
+                       _minimize_adasecant,_minimize_adaQN,_minimize_adaNAQ,_minimize_molnaq,
                        _minimize_newtoncg,_minimize_scalar_brent, _minimize_scalar_bounded,
                        _minimize_scalar_golden, MemoizeJac)
 from ._trustregion_dogleg import _minimize_dogleg
